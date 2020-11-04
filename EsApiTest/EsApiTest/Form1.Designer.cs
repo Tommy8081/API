fdb5fbab326c2a28fd70c8606b445d89d7645bad
Tommy8081 <1247527652@qq.com>
2020-11-04 08:02:25 +0800

    api

---
+++ EsApiTest/EsApiTest/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace EsApiTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.eSStartSessionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.miStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.miGetPic = new System.Windows.Forms.ToolStripMenuItem();
+            this.miSendKey = new System.Windows.Forms.ToolStripMenuItem();
+            this.miSetRemoteMouse = new System.Windows.Forms.ToolStripMenuItem();
+            this.miEndSession = new System.Windows.Forms.ToolStripMenuItem();
+            this.testWithCallbakToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.miStartCallback = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eSStartSessionToolStripMenuItem,
+            this.testWithCallbakToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // eSStartSessionToolStripMenuItem
+            // 
+            this.eSStartSessionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miStart,
+            this.miGetPic,
+            this.miSendKey,
+            this.miSetRemoteMouse,
+            this.miEndSession});
+            this.eSStartSessionToolStripMenuItem.Name = "eSStartSessionToolStripMenuItem";
+            this.eSStartSessionToolStripMenuItem.Size = new System.Drawing.Size(41, 21);
+            this.eSStartSessionToolStripMenuItem.Text = "test";
+            // 
+            // miStart
+            // 
+            this.miStart.Name = "miStart";
+            this.miStart.Size = new System.Drawing.Size(198, 22);
+            this.miStart.Text = "ES_StartSession";
+            this.miStart.Click += new System.EventHandler(this.miStart_Click);
+            // 
+            // miGetPic
+            // 
+            this.miGetPic.Enabled = false;
+            this.miGetPic.Name = "miGetPic";
+            this.miGetPic.Size = new System.Drawing.Size(198, 22);
+            this.miGetPic.Text = "GetPicture";
+            this.miGetPic.Click += new System.EventHandler(this.miGetPic_Click);
+            // 
+            // miSendKey
+            // 
+            this.miSendKey.Enabled = false;
+            this.miSendKey.Name = "miSendKey";
+            this.miSendKey.Size = new System.Drawing.Size(198, 22);
+            this.miSendKey.Text = "ES_SendKey";
+            this.miSendKey.Click += new System.EventHandler(this.miSendKey_Click);
+            // 
+            // miSetRemoteMouse
+            // 
+            this.miSetRemoteMouse.Enabled = false;
+            this.miSetRemoteMouse.Name = "miSetRemoteMouse";
+            this.miSetRemoteMouse.Size = new System.Drawing.Size(198, 22);
+            this.miSetRemoteMouse.Text = "ES_SetRemoteMouse";
+            this.miSetRemoteMouse.Click += new System.EventHandler(this.miSetRemoteMouse_Click);
+            // 
+            // miEndSession
+            // 
+            this.miEndSession.Enabled = false;
+            this.miEndSession.Name = "miEndSession";
+            this.miEndSession.Size = new System.Drawing.Size(198, 22);
+            this.miEndSession.Text = "ES_EndSession";
+            this.miEndSession.Click += new System.EventHandler(this.miEndSession_Click);
+            // 
+            // testWithCallbakToolStripMenuItem
+            // 
+            this.testWithCallbakToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miStartCallback});
+            this.testWithCallbakToolStripMenuItem.Name = "testWithCallbakToolStripMenuItem";
+            this.testWithCallbakToolStripMenuItem.Size = new System.Drawing.Size(113, 21);
+            this.testWithCallbakToolStripMenuItem.Text = "TestWithCallbak";
+            // 
+            // miStartCallback
+            // 
+            this.miStartCallback.Name = "miStartCallback";
+            this.miStartCallback.Size = new System.Drawing.Size(180, 22);
+            this.miStartCallback.Text = "Start";
+            this.miStartCallback.Click += new System.EventHandler(this.miStartCallback_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem eSStartSessionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem miStart;
+        private System.Windows.Forms.ToolStripMenuItem miGetPic;
+        private System.Windows.Forms.ToolStripMenuItem miSendKey;
+        private System.Windows.Forms.ToolStripMenuItem miSetRemoteMouse;
+        private System.Windows.Forms.ToolStripMenuItem miEndSession;
+        private System.Windows.Forms.ToolStripMenuItem testWithCallbakToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem miStartCallback;
+    }
+}
+
---
+++ EsApiTest/EsApiTest/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+/// <summary>
+// 版本：V0.2
+//版本更改：
+//1．	增加x64支持
+//2．	Handle的类型更改为IntPtr
+
+/// </summary>
+namespace EsApiTest
+{
+    public partial class Form1 : Form
+    {
+        /*
+        typedef void(__stdcall *VideoSizeChangedCallback)(int newWidth, int newHeight);
+        typedef void(__stdcall *UpdatePicCallback)(INT_PTR Param);
+        typedef void(__stdcall *ErrorCallback)(int err);
+
+         */
+        public delegate void UpdatePicCallback(IntPtr lParam);
+        public delegate void VideoSizeChangedCallback(int newWidth, int newHeight);
+        public delegate void ErrorCallback(int err);
+
+        /*
+        #define BM_LBUTTON		0x01
+        #define BM_MBUTTON		0x02
+        #define BM_RBUTTON		0x04
+        #define BM_SYNC			0x20
+
+        int WINAPI ES_StartSession(LPCSTR Server, int Port, LPCSTR User, LPCSTR Password, int& Handle);
+        int WINAPI ES_EndSession(int Handle);
+        int WINAPI ES_SendKey(int Handle, unsigned int nChar, int iDown, bool bShiftDown,bool bCapLockDown);
+        int WINAPI ES_SetRemoteMouse(int Handle, int x,int y, int z, BYTE bMask);
+        WORD* WINAPI ES_LockPicture(int Handle, int &Size, int &Width, int &Height);
+        int WINAPI ES_UnlockPicture(int Handle);
+
+	    int WINAPI ES_RegisterVideoSizeChangedCallback(INT_PTR Handle, VideoSizeChangedCallback cb);
+	    int WINAPI ES_RegisterUpdatePicCallback(INT_PTR Handle, UpdatePicCallback cb, INT_PTR param);
+	    int WINAPI ES_RegisterErrCallback(INT_PTR Handle, ErrorCallback cb);
+
+        */
+        [Flags]
+        enum EsMouseBM : byte
+        {
+            BM_NONE = 0x00,
+            BM_LBUTTON = 0x01,
+            BM_MBUTTON = 0x02,
+            BM_RBUTTON = 0x04,
+            BM_SYNC = 0x20
+        }
+        [DllImport("EsApi.dll", EntryPoint = "ES_StartSession", CharSet = CharSet.Ansi)]
+        public static extern int ES_StartSession(string Server, int Port, string User, string Password,ref IntPtr Handle);
+
+        [DllImport("EsApi.dll", EntryPoint = "ES_EndSession", CharSet = CharSet.Ansi)]
+        public static extern int ES_EndSession(IntPtr Handle);
+
+
+        [DllImport("EsApi.dll", EntryPoint = "ES_LockPicture", CharSet = CharSet.Ansi)]
+        public static extern IntPtr ES_LockPicture(IntPtr Handle, ref int Size, ref int Width, ref int Height);
+
+        [DllImport("EsApi.dll", EntryPoint = "ES_UnlockPicture", CharSet = CharSet.Ansi)]
+        public static extern int ES_UnlockPicture(IntPtr Handle);
+
+        [DllImport("EsApi.dll", EntryPoint = "ES_SendKey", CharSet = CharSet.Ansi)]
+        public static extern int ES_SendKey(IntPtr Handle, uint nChar, int iDown, bool bShiftDown, bool bCapLockDown);
+
+        [DllImport("EsApi.dll", EntryPoint = "ES_SetRemoteMouse", CharSet = CharSet.Ansi)]
+        public static extern int ES_SetRemoteMouse(IntPtr Handle, int x, int y, int z, byte bMask);
+
+        [DllImport("EsApi.dll", EntryPoint = "ES_RegisterUpdatePicCallback", CharSet = CharSet.Ansi)]
+        public static extern int ES_RegisterUpdatePicCallback(IntPtr Handle, UpdatePicCallback up_callback, IntPtr param);
+
+        [DllImport("EsApi.dll", EntryPoint = "ES_RegisterVideoSizeChangedCallback", CharSet = CharSet.Ansi)]
+        public static extern int ES_RegisterVideoSizeChangedCallback(IntPtr Handle, VideoSizeChangedCallback sc_callback);
+
+        [DllImport("EsApi.dll", EntryPoint = "ES_RegisterErrCallback", CharSet = CharSet.Ansi)]
+        public static extern int ES_RegisterErrCallback(IntPtr Handle, ErrorCallback er_callback);
+
+
+        private IntPtr hSession;
+        private UpdatePicCallback updatePicCallback;
+        private VideoSizeChangedCallback videoSizeChangedCallback;
+        private ErrorCallback errorCallback;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void miStart_Click(object sender, EventArgs e)
+        {
+            int r = ES_StartSession("192.168.81.11", 6666, "admin", "", ref hSession);
+            Console.WriteLine($"ES_StartSession return {r}");
+            if(r==0)
+            {
+                miStart.Enabled = miStartCallback.Enabled = false;
+                miGetPic.Enabled = miSendKey.Enabled = miSetRemoteMouse.Enabled = miEndSession.Enabled = true;
+            }
+        }
+
+        private void miGetPic_Click(object sender, EventArgs e)
+        {
+            int Size = 0;
+            int Width = 0;
+            int Height = 0;
+            if (hSession == IntPtr.Zero) return;
+            IntPtr pic = ES_LockPicture(hSession, ref Size, ref Width, ref Height);
+            Console.WriteLine($"ES_LockPicture Size={Size},Width={Width},Height={Height}");
+            if (pic != IntPtr.Zero)
+            {
+                try
+                {
+                    using (Graphics g = CreateGraphics())
+                    {
+                        Bitmap img = new Bitmap(Width, Height, Width * 2, PixelFormat.Format16bppRgb565, pic);
+                        g.DrawImage(img, 0, 0);
+                    }
+                }
+                finally
+                {
+                    int r = ES_UnlockPicture(hSession);
+                    Console.WriteLine($"ES_UnlockPicture return {r}");
+                }
+            }
+        }
+
+        private void miSendKey_Click(object sender, EventArgs e)
+        {
+            int r=ES_SendKey(hSession, 'A', 1, false, false);
+            Console.WriteLine($"ES_SendKey return {r}");
+            r = ES_SendKey(hSession, 'A',0, false, false);
+            Console.WriteLine($"ES_SendKey return {r}");
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void miSetRemoteMouse_Click(object sender, EventArgs e)
+        {
+            int r=ES_SetRemoteMouse(hSession, 100, 100, 0, 0x20);
+            Console.WriteLine($"ES_SetRemoteMouse return {r}");
+        }
+
+        private void miEndSession_Click(object sender, EventArgs e)
+        {
+            int r=ES_EndSession(hSession);
+            Console.WriteLine($"ES_EndSession return {r}");
+            hSession = IntPtr.Zero;
+            miStart.Enabled = miStartCallback.Enabled = true;
+            miGetPic.Enabled = miSendKey.Enabled = miSetRemoteMouse.Enabled = miEndSession.Enabled = false;
+
+        }
+        void OnUpdatePic(IntPtr lParam)
+        {           
+            int Size = 0;
+            int Width = 0;
+            int Height = 0;
+            if (this.WindowState == FormWindowState.Minimized) return;
+            IntPtr pic = ES_LockPicture(hSession, ref Size, ref Width, ref Height);
+            Console.WriteLine($"ES_LockPicture Size={Size},Width={Width},Height={Height}");
+            if (pic != IntPtr.Zero)
+            {
+                try
+                {
+                    using (Graphics g = CreateGraphics())
+                    {
+                        Bitmap img = new Bitmap(Width, Height, Width * 2, PixelFormat.Format16bppRgb565, pic);
+                        g.DrawImage(img, 0, 0);
+                    }
+                }
+                finally
+                {
+                    int r = ES_UnlockPicture(hSession);
+                    Console.WriteLine($"ES_UnlockPicture return {r}");
+                }
+            }
+            
+        }
+        void OnVideoSizeChanged(int newWidth, int newHeight)
+        {
+            Console.WriteLine($"OnVideoSizeChanged newWidth={newWidth},newHeight={newHeight}");
+        }
+        void OnError(int err)
+        {
+            Console.WriteLine($"OnError Err={err}");
+        }
+
+        private void miStartCallback_Click(object sender, EventArgs e)
+        {
+            int r = ES_StartSession("192.168.81.11", 6666, "admin", "", ref hSession);
+            Console.WriteLine($"ES_StartSession return {r}");
+            if (r == 0)
+            {
+                updatePicCallback = new UpdatePicCallback(OnUpdatePic);
+                ES_RegisterUpdatePicCallback(hSession, updatePicCallback, IntPtr.Zero);
+                videoSizeChangedCallback = new VideoSizeChangedCallback(OnVideoSizeChanged);
+                ES_RegisterVideoSizeChangedCallback(hSession, videoSizeChangedCallback);
+                errorCallback = new ErrorCallback(OnError);
+                ES_RegisterErrCallback(hSession, errorCallback);
+
+                miStart.Enabled= miStartCallback.Enabled = false;
+                miGetPic.Enabled = miSendKey.Enabled = miSetRemoteMouse.Enabled = miEndSession.Enabled = true;
+            }
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            int Size = 0;
+            int Width = 0;
+            int Height = 0;
+            if (hSession == IntPtr.Zero) return;
+            
+            IntPtr pic = ES_LockPicture(hSession, ref Size, ref Width, ref Height);
+            Console.WriteLine($"ES_LockPicture Size={Size},Width={Width},Height={Height}");
+            if (pic != IntPtr.Zero)
+            {
+                try
+                {
+                   Bitmap img = new Bitmap(Width, Height, Width * 2, PixelFormat.Format16bppRgb565, pic);
+                   e.Graphics.DrawImage(img, 0, 0);
+                }
+                finally
+                {
+                    int r = ES_UnlockPicture(hSession);
+                    Console.WriteLine($"ES_UnlockPicture return {r}");
+                }
+            }
+
+        }
+    }
+}
